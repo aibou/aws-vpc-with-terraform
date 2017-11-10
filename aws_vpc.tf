2efc9816f19161971097a77765f54fda693bb65e
aibou <aibou7251@gmail.com>
2017-11-10 16:12:40 +0900

    Fix for new version of terraform, attach VPC Endpoint(S3) to private subnet

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -1,5 +1,5 @@
 data "template_file" "vpc-name" {
-  template = "vpc-${env}-${pj}"
+  template = "vpc-$${env}-$${pj}"
   vars {
     env = "${var.environment}"
     pj = "${var.project_name}"
@@ -113,6 +113,11 @@ resource "aws_route_table" "rtbl-pri-c" {
   }
 }
 
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  service_name = "com.amazonaws.${var.aws_region}.s3"
+}
+
 resource "aws_route_table_association" "rtbl-asc-pri-a" {
   subnet_id      = "${aws_subnet.subnet-pri-a.id}"
   route_table_id = "${aws_route_table.rtbl-pri-a.id}"
@@ -121,6 +126,16 @@ resource "aws_route_table_association" "rtbl-asc-pri-a" {
 resource "aws_route_table_association" "rtbl-asc-pri-c" {
   subnet_id      = "${aws_subnet.subnet-pri-c.id}"
   route_table_id = "${aws_route_table.rtbl-pri-c.id}"
+}
+
+resource "aws_vpc_endpoint_route_table_association" "ve-pri-a" {
+  vpc_endpoint_id = "${aws_vpc_endpoint.s3.id}"
+  route_table_id  = "${aws_route_table.rtbl-pri-a.id}"
+}
+
+resource "aws_vpc_endpoint_route_table_association" "ve-pri-c" {
+  vpc_endpoint_id = "${aws_vpc_endpoint.s3.id}"
+  route_table_id  = "${aws_route_table.rtbl-pri-c.id}"
 }
 
 resource "aws_eip" "eip-ngw-a" {
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 data "template_file" "ngw_ips" {
-  template = "${az_a},${az_c}"
+  template = "$${az_a},$${az_c}"
   vars {
     az_a = "${aws_eip.eip-ngw-a.public_ip}"
     az_c = "${aws_eip.eip-ngw-c.public_ip}"
